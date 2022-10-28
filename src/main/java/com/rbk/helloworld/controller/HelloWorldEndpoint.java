e21692d2b0957a1c5a7157d42675d48f89eaa0c2
Ramzi Ben Khedhiri <bk.ramzi@gmail.com>
2022-10-28 19:38:37 +0200

    implement HelloWorldEndpoint

---
+++ src/main/java/com/rbk/helloworld/controller/HelloWorldEndpoint.java
@@ -0,0 +1,22 @@
+package com.rbk.helloworld.controller;
+
+import org.hibernate.validator.constraints.Length;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+
+
+@RestController
+@Validated
+public class HelloWorldEndpoint {
+
+    @GetMapping("/hello")
+    public ResponseEntity getMessage(@RequestParam("name") @Length(max = 30) String name, @RequestParam("age") @Max(200) @Min(0) int age) {
+        return ResponseEntity.ok("Hello " + name + " "+age);
+    }
+}
