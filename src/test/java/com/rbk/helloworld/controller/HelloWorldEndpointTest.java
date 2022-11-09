c28db8cd18d09fa95e3fe09ebad00bb440391542
Ramzi Ben Khedhiri <bk.ramzi@gmail.com>
2022-11-09 21:13:03 +0100

    unit test hello endpoint

--- src/test/java/com/rbk/helloworld/controller/HelloWorldEndpointTest.java
+++ src/test/java/com/rbk/helloworld/controller/HelloWorldEndpointTest.java
@@ -1,11 +1,9 @@
 package com.rbk.helloworld.controller;
 
-import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.*;
-import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
 
 
@@ -19,13 +17,13 @@ class HelloWorldEndpointTest {
     @Test
     void getMessage() {
         given().contentType(ContentType.TEXT)
-                .queryParam("name","ramzi")
+                .queryParam("name","John")
                 .queryParam("age","20")
                 .when()
                 .get("/hello")
                 .then()
                 .statusCode(200)
-                .body(equalTo("Hello ramzi 20"));
+                .body(equalTo("Hello John 20"));
     }
 
 }
