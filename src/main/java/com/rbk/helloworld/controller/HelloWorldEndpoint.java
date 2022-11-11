923ca2b5e6b7a9a5863bd9d203c25f2b8229d5fb
Ramzi Ben Khedhiri <bk.ramzi@gmail.com>
2022-11-11 23:31:52 +0100

    implement post message endpoint

--- src/main/java/com/rbk/helloworld/controller/HelloWorldEndpoint.java
+++ src/main/java/com/rbk/helloworld/controller/HelloWorldEndpoint.java
@@ -3,12 +3,11 @@ package com.rbk.helloworld.controller;
 import org.hibernate.validator.constraints.Length;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
 
 
 @RestController
@@ -18,5 +17,11 @@ public class HelloWorldEndpoint {
     @GetMapping("/hello")
     public ResponseEntity<String> getMessage(@RequestParam("name") @Length(max = 30) String name, @RequestParam("age") @Max(200) @Min(0) int age) {
         return ResponseEntity.ok("Hello " + name + " "+age);
+    }
+
+    @PostMapping("/message")
+    @ResponseBody
+    public ResponseEntity<String> postMessage(@RequestBody @NotEmpty String message) {
+        return ResponseEntity.ok("Your message was " + message);
     }
 }
--- src/test/java/com/rbk/helloworld/controller/HelloWorldEndpointTest.java
+++ src/test/java/com/rbk/helloworld/controller/HelloWorldEndpointTest.java
@@ -1,10 +1,15 @@
 package com.rbk.helloworld.controller;
 
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import static io.restassured.RestAssured.*;
-import static org.hamcrest.Matchers.*;
+
+import static io.restassured.RestAssured.basePath;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 
 class HelloWorldEndpointTest {
@@ -24,6 +29,20 @@ class HelloWorldEndpointTest {
                 .then()
                 .statusCode(200)
                 .body(equalTo("Hello John 20"));
+    }
+
+    @Test
+    void postMessge() {
+        String message = ".... . .-.. .-.. --- / .-- --- .-. .-.. -..";
+
+        given()
+                .contentType(ContentType.JSON)
+                .body(message)
+                .when()
+                .post("/message")
+                .then()
+                .statusCode(200)
+                .body(equalTo("Your message was .... . .-.. .-.. --- / .-- --- .-. .-.. -.."));
     }
 
 }
